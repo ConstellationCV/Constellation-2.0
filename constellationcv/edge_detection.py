ae210ccfd07b941570c68631b0a73a5501791412
prathgan <prathamgandhi4@gmail.com>
2018-08-20 08:57:57 +0530

    committing

--- constellationcv/edge_detection.py
+++ constellationcv/edge_detection.py
@@ -2,6 +2,7 @@ import cv2
 from arithmetic_toolkit import Matrices
 from arithmetic_toolkit import Vectors
 import numpy as np
+
 class edge_detector(object):
 	"""class which contains all functions to extract edges from an image"""
 	def __init__(self, imagePath):
@@ -15,10 +16,11 @@ class edge_detector(object):
 		img = self.img_rgb
 		num_rows = len(img)
 		num_cols = len(img[0])
-		edge_mat = np.zeros(shape=(num_rows,num_cols))
+		edge_mat = np.around(np.zeros(shape=(num_rows,num_cols)),decimals=0)
 		row_count = 0
 		col_count = 0
 		threshold=7
+		reached = False
 		for row in img:
 			for col in row:
 				if row_count>=num_rows-1 or col_count>=num_cols-1 or row_count==0 or col_count==0:
@@ -31,3 +33,5 @@ class edge_detector(object):
 			col_count = 0
 			row_count+=1
 		return edge_mat
+
+	def calculateBestFit
--- tests/edge_detection_test.py
+++ tests/edge_detection_test.py
@@ -2,4 +2,7 @@ import constellationcv as cv
 import numpy as np
 
 e = cv.edge_detector("image.png")
-print(np.where(e.findEdgeMatrix()==1))
+# print(np.where(e.findEdgeMatrix()==1))
+edgeMat = e.findEdgeMatrix()
+# np.savetxt('outputmat.txt', edgeMat)
+print edgeMat[0][0]
