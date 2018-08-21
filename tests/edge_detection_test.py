d9eb5b4e2b2386b0aac51dba324708401b3fcff8
prathgan <prathamgandhi4@gmail.com>
2018-08-21 16:30:25 +0530

    abandon graph approach

--- tests/edge_detection_test.py
+++ tests/edge_detection_test.py
@@ -1,11 +1,11 @@
 import constellationcv as cv
 import numpy as np
 
-e = cv.edge_detector_2("cube.png")
-# print(np.where(e.findEdgeMatrix()==1))
+e = cv.edge_detector("cube.png")
 edgeMat = e.findEdgeMatrix()
-e.outputEdgeImage(edgeMat,"edges_output.png")
-e.printEquations(e.formAllLines(edgeMat))
+print e.createEdgePointsList(edgeMat)
+# e.outputEdgeImage(edgeMat,"edges_output.png")
+# e.printEquations(e.formAllLines(edgeMat))
 # np.savetxt('outputmat.txt', edgeMat)
 #print edgeMat[0][0]
 # print(e.calculateBestFit([[1,1],[3,3],[10,6],[13,16]]))
